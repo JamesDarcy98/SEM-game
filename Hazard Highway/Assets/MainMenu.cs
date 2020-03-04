3beeb33eb7624741a3a66d92d03f1d5b7a229f66
AdamHowlett1 <pmxah20@nottingham.ac.uk>
2020-03-04 14:07:43 +0000

    Final with rename

---
+++ Hazard Highway/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Awake(){
+         PlayerPrefs.SetInt("CarChoice", 1);
+    }
+
+    public void PlayGame(){
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame(){
+        //Debug.Log("Quit");
+        Application.Quit();
+    }
+}
